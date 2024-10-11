ca1708ca1983eade7412e91783b8a2669c2a316c
Jim Choy <choyjim33@gmail.com>
2024-10-11 17:01:29 -0400

    small changes to the response.php page for lab 5

--- response.php
+++ response.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Here is the Form Response</title>
+        <title>Form Response</title>
     </head>
     <body>
         <!--<p>GET: <
         //?= var_dump ($_GET) ?>
         </p>-->
-        <h2>Personal Information</h2>
-        <br/>
+        <h1>Personal Information</h1>
         <p>Thank you for entrusting me with your personal information; it fills me with gratitude. </p><br/>
         <p>Your name is <?= htmlspecialchars ($_POST ['firstname']) . " " . htmlspecialchars ($_POST ['lastname']) ?></p>
         <p>Your birthday is on <?= htmlspecialchars ($_POST ['bdate']) ?></p>
-        <br/>
-        <h3>Vital information to take not of</h3>
+        <h3>Vital information to take not of...</h3>
         <p>Credit Card Number: <?= htmlspecialchars ($_POST ['credit']) ?></p>
         <p>Expiration Date: <?= htmlspecialchars ($_POST ['date']) ?></p>
         <p>cvv: <?= htmlspecialchars ($_POST ['cvv']) ?></p>
+        <h3>Funsies :> </h3>
+        <p>Chicken Preference --> <?= htmlspecialchars ($_POST ['favourite chicken']) ?></p>
     </body>
 </html>
