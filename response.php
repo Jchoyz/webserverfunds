c437476f44d6000c68d075a4bc8b21666ec275e8
Jim Choy <choyjim33@gmail.com>
2024-10-11 17:07:19 -0400

    last change

--- response.php
+++ response.php
@@ -9,7 +9,7 @@
         </p>-->
         <h1>Important Stuff to take into Consideration</h1>
         <p>Thank you for entrusting me with your personal information; it fills me with gratitude. </p><br/>
-        <h3>Personal Information<h3>
+        <h3>Personal Information</h3>
         <p>Your name is <?= htmlspecialchars ($_POST ['firstname']) . " " . htmlspecialchars ($_POST ['lastname']) ?></p>
         <p>Your birthday is on <?= htmlspecialchars ($_POST ['bdate']) ?></p>
         <h3>Vital information to take not of...</h3>
@@ -17,6 +17,6 @@
         <p>Expiration Date: <?= htmlspecialchars ($_POST ['date']) ?></p>
         <p>cvv: <?= htmlspecialchars ($_POST ['cvv']) ?></p>
         <h3>Funsies :> </h3>
-        <p>Chicken Preference --> <?= htmlspecialchars ($_POST ['favouritechicken']) ?></p>
+        <!--<p>Chicken Preference -> <//?= htmlspecialchars ($_POST ['favouritechicken']) ?></p> -->
     </body>
 </html>
