b1a8fa49c157e0d1c3af7436450a0e560f693487
Jim Choy <choyjim33@gmail.com>
2024-10-11 16:52:13 -0400

    Rewording the response.php page

--- response.php
+++ response.php
@@ -1,7 +1,21 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Here is the Form Response</title>
+    </head>
     <body>
-        <p>GET: <?= var_dump ($_GET) ?></p>
-        <p>POST: <?= var_dump ($_POST) ?></p>
+        <!--<p>GET: <
+        //?= var_dump ($_GET) ?>
+        </p>-->
+        <h2>Personal Information</h2>
+        <br/>
+        <p>Thank you for entrusting me with your personal information; it fills me with gratitude. </p><br/>
+        <p>Your name is <?= htmlspecialchars ($_POST ['firstname']) . " " . htmlspecialchars ($_POST ['lastname']) ?></p>
+        <p>Your birthday is on <?= htmlspecialchars ($_POST ['bdate']) ?></p>
+        <br/>
+        <h3>Vital information to take not of</h3>
+        <p>Credit Card Number: <?= htmlspecialchars ($_POST ['credit']) ?></p>
+        <p>Expiration Date: <?= htmlspecialchars ($_POST ['date']) ?></p>
+        <p>cvv: <?= htmlspecialchars ($_POST ['cvv']) ?></p>
     </body>
 </html>
