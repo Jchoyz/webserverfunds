76704c361bc87abc380880b1214ef1d11191eb9d
Jim Choy <choyjim33@gmail.com>
2024-10-12 13:05:18 -0400

    Changes to the response.php file -- cURL

--- response.php
+++ response.php
@@ -4,18 +4,18 @@
         <title>Form Response</title>
     </head>
     <body>
-        <!--<p>GET: <
-        //?= var_dump ($_GET) ?>
-        </p>-->
         <h1>Important Stuff to take into Consideration</h1>
         <p>Thank you for entrusting me with your personal information; it fills me with gratitude. </p><br/>
+        
         <h3>Personal Information</h3>
         <p>Your name is <?= htmlspecialchars ($_POST ['firstname']) . " " . htmlspecialchars ($_POST ['lastname']) ?></p>
         <p>Your birthday is on <?= htmlspecialchars ($_POST ['bdate']) ?></p>
-        <h3>Vital information to take not of...</h3>
+
+        <h3>Vital information to take note of...</h3>
         <p>Credit Card Number: <?= htmlspecialchars ($_POST ['credit']) ?></p>
         <p>Expiration Date: <?= htmlspecialchars ($_POST ['date']) ?></p>
         <p>cvv: <?= htmlspecialchars ($_POST ['cvv']) ?></p>
+
         <h3>Funsies :> </h3>
         <!--<p>Chicken Preference -> <//?= htmlspecialchars ($_POST ['favouritechicken']) ?></p> -->
     </body>
